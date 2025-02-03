5016c9e82e3fbd6bc9d204f83721d49f26618321
Юрий <doomov@yandex.ru>
2025-02-04 05:09:22 +0700

    Add files via upload

---
+++ ConsoleAsynchronousTransactions 001/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleAsynchronousTransactions_001
+{
+	internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+			// Создаем новый экземпляр класса Worker
+			var worker = new Worker();
+
+			// Запускаем асинхронный метод DoWork
+			var doWorkTask = worker.DoWork();
+
+			// Запускаем асинхронный метод PrintPoints для вывода точек в консоль
+			var printPointsTask = Worker.PrintPoints(worker);
+
+			// Ожидаем завершения обоих задач
+			await Task.WhenAll(doWorkTask, printPointsTask);
+			Console.WriteLine();
+        }
+    }
+}
---
+++ ConsoleAsynchronousTransactions 001/Worker.cs
@@ -0,0 +1,65 @@
+﻿namespace ConsoleAsynchronousTransactions_001
+{
+	/// <summary>
+	/// Представляет рабочий процесс в многопоточном окружении.
+	/// </summary>
+	public class Worker
+	{
+		/// <summary>
+		/// Указывает, завершена ли работа. 
+		/// Ключевое слово volatile означает, что поле может изменяться несколькими потоками одновременно.
+		/// </summary>
+		private volatile bool _isComplete;
+
+		/// <summary>
+		/// Получает или задает значение, указывающее, завершена ли работа.
+		/// </summary>
+		public bool IsComplete
+		{
+			get => _isComplete;
+			set => _isComplete = value;
+		}
+
+		/// <summary>
+		/// Выполняет работу асинхронно.
+		/// </summary>
+		/// <returns>Task, представляющая асинхронную операцию.</returns>
+		public async Task DoWork()
+		{
+			IsComplete = false;
+			Console.WriteLine("Doing work");
+
+			await LongOperation();
+			Console.WriteLine();
+
+			Console.WriteLine("Work completed");
+
+			IsComplete = true;
+		}
+
+		/// <summary>
+		/// Выполняет длительную операцию с асинхронной задержкой.
+		/// </summary>
+		/// <returns>Task, представляющая асинхронную операцию.</returns>
+		private async Task LongOperation()
+		{
+			Console.WriteLine("Working!");
+			await Task.Delay(2000); // Используем асинхронную задержку
+		}
+
+		/// <summary>
+		/// Печатает точки в консоль, пока работа не будет завершена.
+		/// </summary>
+		/// <param name="worker">Объект Worker, который выполняет работу.</param>
+		/// <returns>Task, представляющая асинхронную операцию.</returns>
+		public static async Task PrintPoints(Worker worker)
+		{
+			while (!worker.IsComplete)
+			{
+				Console.Write(".");
+				await Task.Delay(100);
+			}
+			Console.WriteLine();
+		}
+	}
+}
